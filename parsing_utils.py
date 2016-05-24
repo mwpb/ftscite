c6adc29408eb1f52d99f67fb9ab5570d74d41421
Matthew Burke <matthew.burke@cantab.net>
2016-05-24 22:21:16 +0100

    automatically removes %20

--- parsing_utils.py
+++ parsing_utils.py
@@ -44,7 +44,7 @@ def extract_entries(bib_path):
 
 def search(phrase):
     phrase_list = phrase.split(' ')
-    query = (Entry.select(Entry).join(EntryIndex,on=(Entry.id == EntryIndex.docid)).where(EntryIndex.content.in_(**phrase_list)).dicts())
+    query = (Entry.select(Entry).join(EntryIndex,on=(Entry.id == EntryIndex.docid)).where(EntryIndex.content.in_(*phrase_list)).dicts())
     search_results = []
     for row_dict in query:
         search_results.append(row_dict)
