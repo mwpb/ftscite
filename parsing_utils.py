850ea370b13acf7322102b7a86621b1075701015
Matthew Burke <matthew.burke@cantab.net>
2016-05-25 18:54:34 +0100

    return to single phrase

--- parsing_utils.py
+++ parsing_utils.py
@@ -45,7 +45,7 @@ def extract_entries(bib_path):
 def search(phrase):
     phrase_list = phrase.split(' ')
     search_results = []
-    query = (Entry.select(Entry).join(EntryIndex,on=(Entry.id == EntryIndex.docid)).where(EntryIndex.content.contains(term)).dicts())
+    query = (Entry.select(Entry).join(EntryIndex,on=(Entry.id == EntryIndex.docid)).where(EntryIndex.content.contains(phrase)).dicts())
     for row_dict in query:
         search_results.append(row_dict)
     return search_results
